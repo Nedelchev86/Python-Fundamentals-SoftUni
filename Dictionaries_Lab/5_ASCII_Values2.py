0a518cccc891d2d099a8a466fb802bb68dd9bd2a
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-24 16:21:31 +0200

    Dictionaries Labs Exercise
    Dictionaries Labs Exercise

---
+++ Dictionaries_Lab/5_ASCII_Values2.py
@@ -0,0 +1,8 @@
+# data = input().split(", ")
+# my_dict = {x: ord(x) for x in data}
+# print(my_dict)
+
+
+print({x: ord(x) for x in input().split(", ")})
+
+
---
+++ Dictionaries_Lab/5_ASCII_Values3.py
@@ -0,0 +1,3 @@
+data = input().split(", ")
+my_dict = {x: ord(x) for x in data}
+print(my_dict)
