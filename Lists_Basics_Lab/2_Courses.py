a13574c4b3d1f0c5d1afc982ec6d62a6b7948473
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-11-30 06:26:32 +0200

    Python Lists advanced exercise
    Python Lists advanced exercise

---
+++ Lists_Basics_Lab/2_Courses.py
@@ -0,0 +1,8 @@
+num = int(input())
+courses = []
+
+for _ in range(num):
+    words = input()
+    courses.append(words)
+print(courses)
+
---
+++ Lists_Basics_Lab/2_Courses2.py
@@ -0,0 +1,7 @@
+num = int(input())
+courses = []
+
+for _ in range(num):
+    courses.append(input())
+print(courses)
+
