2b50254d5215b1141a6a5cb23138814f034b8260
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-06 16:08:37 +0200

    Python text processing
    Python text processing

---
+++ Text_Processin_ Exercise/4_Caesar Cipher.py
@@ -0,0 +1,9 @@
+# text = input()
+# new_text = ""
+#
+# for i in text:
+#     new_text += chr((ord(i) + 3))
+# print(new_text)
+
+# Just test
+print("".join([chr(ord(x) +3 ) for x in input()]))
---
+++ Text_Processin_ Exercise/4_Caesar Cipher2.py
@@ -0,0 +1,1 @@
+print("".join([chr(ord(x)+3) for x in input()]))
